c20e1dfe30657864bb268897b61e91d78270a1e3
Nick Prater <NikPratr@gmail.com>
2022-09-01 21:35:49 -0400

    Completed challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+writePassword = function() {
+
 var lowercase   = "abcdefghijklmnopqrstuvwxyz"
-var uppercase   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var nums        = "0123456789"
-var symbols     = "!@#$%^&*()"
+var uppercase   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var nums        = "01234567890123456789"
+var symbols     = "!@#$%^&*()!@#$%^&*()"
+var passLibrary = ""
 var newPassword = ""
 
-var c1 = window.prompt("How many characters do you want your password to be? (8-25)");
-var c2 = window.confirm("Would you like uppercase letters? (Press ok for yes or cancel for no)");
-var c3 = window.confirm("Would you like numbers? (Press ok for yes or cancel for no)");
-var c4 = window.confirm("Would you like symbols? (Press ok for yes or cancel for no)");
-
-if(c1 >=8 && c1 <= 25) {
+  var c1 = window.prompt("How many characters do you want your password to be? (8-25)");
+  var c2 = window.confirm("Would you like lowercase letters? (Press ok for yes or cancel for no)");
+  var c3 = window.confirm("Would you like uppercase letters? (Press ok for yes or cancel for no)");
+  var c4 = window.confirm("Would you like numbers? (Press ok for yes or cancel for no)");
+  var c5 = window.confirm("Would you like symbols? (Press ok for yes or cancel for no)");
+  
+  if(c1 >=8 && c1 <= 25) {
     var passLength = c1;
   } else {
     var passLength = 8;
   }
 
 if (c2) {
-  var passLibrary = lowercase.concat(uppercase);
-} else {
-  var passLibrary = lowercase;
+  passLibrary = passLibrary.concat(lowercase);
 }
 
 if (c3) {
+  passLibrary = lowercase.concat(uppercase);
+}
+
+if (c4) {
   passLibrary = passLibrary.concat(nums);
 }
 
-if(c4) {
+if(c5) {
   passLibrary = passLibrary.concat(symbols);
+}
+
+if(!c2 && !c3 && !c4 && !c5) {
+  return window.confirm("You have no password!")
+}
+
+function shuffle(passLibrary) {
+  var xyz = passLibrary.split("");
+  xyz.sort(function() {
+    return 0.5 - Math.random();
+  });
+
+  passLibrary = xyz.join("");
+  return passLibrary;
 }
 
 for(var i=0; i < passLength; i++) {
   var index = Math.floor(Math.random() * passLibrary.length);
-  newPassword + passLibrary[index];
+  newPassword = newPassword.concat(passLibrary[index]);
   console.log(newPassword);
 }
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-}
+  window.alert("Your new password is " + newPassword)
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
